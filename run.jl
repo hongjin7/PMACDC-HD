cb388c6de5c357127ec90fb31928afaa5323d734
hongjin7 <hongjin.du@outlook.com>
2025-02-28 15:21:56 +0100

    deleted load 43

--- run.jl
+++ run.jl
@@ -21,25 +21,25 @@ bus_data = result["solution"]["bus"]
 conv_data = result["solution"]["convdc"]
 busdc_data = result["solution"]["busdc"]
 
-# # Create DataFrames
-# gen_df = DataFrame(gen_id = collect(keys(gen_data)), pg = [x["pg"] for x in values(gen_data)], qg = [x["qg"] for x in values(gen_data)])
-# bus_df = DataFrame(bus_id = collect(keys(bus_data)), va = [x["va"] for x in values(bus_data)], vm = [x["vm"] for x in values(bus_data)])
-# #conv_df = DataFrame(conv_id = collect(keys(conv_data)), pconv = [x["pconv"] for x in values(conv_data)])
- conv_df = DataFrame(conv_id = collect(keys(conv_data)), droop = [x["droop"] for x in values(conv_data)], pconv = [x["pconv"] for x in values(conv_data)])
-# #conv_df =  DataFrame(conv_id = collect(keys(conv_data)), droop = [x["droop"] for x in values(conv_data)], pconv = [x["pconv"] for x in values(conv_data)], kfdr =  [x["k_fdroop"] for x in values(conv_data)])
-# busdc_df = DataFrame(bus_id = collect(keys(busdc_data)), vdc = [x["vm"] for x in values(busdc_data)])
+# Create DataFrames
+gen_df = DataFrame(gen_id = collect(keys(gen_data)), pg = [x["pg"] for x in values(gen_data)], qg = [x["qg"] for x in values(gen_data)])
+bus_df = DataFrame(bus_id = collect(keys(bus_data)), va = [x["va"] for x in values(bus_data)], vm = [x["vm"] for x in values(bus_data)])
+#conv_df = DataFrame(conv_id = collect(keys(conv_data)), pconv = [x["pconv"] for x in values(conv_data)])
+conv_df = DataFrame(conv_id = collect(keys(conv_data)), droop = [x["droop"] for x in values(conv_data)], pconv = [x["pconv"] for x in values(conv_data)])
+#conv_df =  DataFrame(conv_id = collect(keys(conv_data)), droop = [x["droop"] for x in values(conv_data)], pconv = [x["pconv"] for x in values(conv_data)], kfdr =  [x["k_fdroop"] for x in values(conv_data)])
+busdc_df = DataFrame(bus_id = collect(keys(busdc_data)), vdc = [x["vm"] for x in values(busdc_data)])
 
-# # Create a new Excel workbook
-# XLSX.openxlsx("output.xlsx", mode="w") do xf
-#     sheet_gen = XLSX.addsheet!(xf, "Gen")
-#     XLSX.writetable!(sheet_gen, gen_df)
-#     sheet_bus = XLSX.addsheet!(xf, "Bus")
-#     XLSX.writetable!(sheet_bus, bus_df)
-#     sheet_conv = XLSX.addsheet!(xf, "Conv")
-#     XLSX.writetable!(sheet_conv, conv_df)
-#     sheet_busdc = XLSX.addsheet!(xf, "BusDC")
-#     XLSX.writetable!(sheet_busdc, busdc_df)
-# end
+# Create a new Excel workbook
+XLSX.openxlsx("output.xlsx", mode="w") do xf
+    sheet_gen = XLSX.addsheet!(xf, "Gen")
+    XLSX.writetable!(sheet_gen, gen_df)
+    sheet_bus = XLSX.addsheet!(xf, "Bus")
+    XLSX.writetable!(sheet_bus, bus_df)
+    sheet_conv = XLSX.addsheet!(xf, "Conv")
+    XLSX.writetable!(sheet_conv, conv_df)
+    sheet_busdc = XLSX.addsheet!(xf, "BusDC")
+    XLSX.writetable!(sheet_busdc, busdc_df)
+end
 
 
 
