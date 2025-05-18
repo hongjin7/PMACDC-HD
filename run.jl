56b723fc7d694dc74da018c6c86f4d56e52ee4ae
hongjin7 <hongjin.du@outlook.com>
2025-05-18 13:11:26 +0200

    change run file

--- run.jl
+++ run.jl
@@ -8,14 +8,13 @@ using XLSX
 model = Model(Ipopt.Optimizer)
 #register(model, :sign, 1, sign; autodiff = true)
 
-#result = run_acdcopf("test/data/case9.m", ACPPowerModel, Ipopt.Optimizer)
-result = run_acdcopf("test/data/case9_2grids.m", ACPPowerModel, Ipopt.Optimizer)
+result = run_acdcopf("test/data/case9_acdc_2000MVAdc_4000MVAac.m", ACPPowerModel, Ipopt.Optimizer)
 #result = run_acdcopf("test/data/case32_g16out.m", ACPPowerModel, Ipopt.Optimizer);
 #result = run_acdcopf("test/data/case32_2conv4outage.m", ACPPowerModel, Ipopt.Optimizer);
 #result = run_acdcopf("test/data/case32_1.m", ACPPowerModel, Ipopt.Optimizer)
 #result = run_acdcopf("test/data/case32_2cutbr.m", ACPPowerModel, Ipopt.Optimizer);
 #result = run_acdcopf("test/data/case32_2.m", ACPPowerModel, Ipopt.Optimizer);
-#result = run_acdcopf("test/data/case32_2dconv.m", ACPPowerModel, Ipopt.Optimizer);
+#result = run_acdcopf("test/data/case32_2dconv.m", ACPPowerModel, Ipopt.Optimizer);#
 # Load the data
 gen_data = result["solution"]["gen"]
 bus_data = result["solution"]["bus"]
@@ -30,17 +29,17 @@ conv_df = DataFrame(conv_id = collect(keys(conv_data)), pconv = [x["pconv"] for 
 #conv_df =  DataFrame(conv_id = collect(keys(conv_data)), droop = [x["droop"] for x in values(conv_data)], pconv = [x["pconv"] for x in values(conv_data)], kfdr =  [x["k_fdroop"] for x in values(conv_data)])
 busdc_df = DataFrame(bus_id = collect(keys(busdc_data)), vdc = [x["vm"] for x in values(busdc_data)])
 
-# # Create a new Excel workbook
-# XLSX.openxlsx("output.xlsx", mode="w") do xf
-#     sheet_gen = XLSX.addsheet!(xf, "Gen")
-#     XLSX.writetable!(sheet_gen, gen_df)
-#     sheet_bus = XLSX.addsheet!(xf, "Bus")
-#     XLSX.writetable!(sheet_bus, bus_df)
-#     sheet_conv = XLSX.addsheet!(xf, "Conv")
-#     XLSX.writetable!(sheet_conv, conv_df)
-#     sheet_busdc = XLSX.addsheet!(xf, "BusDC")
-#     XLSX.writetable!(sheet_busdc, busdc_df)
-# end
+# Create a new Excel workbook
+XLSX.openxlsx("output.xlsx", mode="w") do xf
+    sheet_gen = XLSX.addsheet!(xf, "Gen")
+    XLSX.writetable!(sheet_gen, gen_df)
+    sheet_bus = XLSX.addsheet!(xf, "Bus")
+    XLSX.writetable!(sheet_bus, bus_df)
+    sheet_conv = XLSX.addsheet!(xf, "Conv")
+    XLSX.writetable!(sheet_conv, conv_df)
+    sheet_busdc = XLSX.addsheet!(xf, "BusDC")
+    XLSX.writetable!(sheet_busdc, busdc_df)
+end
 
 
 
